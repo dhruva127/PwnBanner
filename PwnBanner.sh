10761d67b3b321c3f23dd2e2eae87cb232462b4f
Dhruv <dhruv00a15@gmail.com>
2025-12-10 11:38:49 +0530

    Create PwnBanner.sh to display a welcome message
    This script reads quotes from a file and displays a welcome message with a random quote.

---
+++ PwnBanner.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+REDCOLOR='\033[31m'
+NC='\033[0m'
+QOUTECOLOR='\033[1;3;36m'
+WelcomeCOLOR='\033[1;33m'
+
+i=0
+
+# Read file line by line into array
+while IFS= read -r line
+do
+    arr[$i]="$line"
+    ((i++))
+done < /home/dhruv/Bash/quotes.txt 
+
+# Generate a random index
+random=$((RANDOM % i))
+
+echo -e "${REDCOLOR}-------------------------------------------------------------------"
+echo -e "${WelcomeCOLOR}============= Welcome back $(whoami) ============="
+echo -e "${REDCOLOR}-------------------------------------------------------------------"
+echo -e "${QOUTECOLOR}${arr[$random]}\n"
+echo -e "${REDCOLOR}-------------------------------------------------------------------${NC}"
